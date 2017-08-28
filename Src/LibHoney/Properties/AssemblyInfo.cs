8f008ef9b963b8d1982ea62312eed8e6c2faa886
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-08-28 02:06:12 +0200

    Prepare the assemblies for Nuget integration.
    * Sign the assemblies under the Release profile.
    * Nuget spec file.

--- Src/LibHoney/Properties/AssemblyInfo.cs
+++ Src/LibHoney/Properties/AssemblyInfo.cs
@@ -12,7 +12,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 
+#if DEBUG
 [assembly: InternalsVisibleTo ("LibHoney.Tests")]
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+#else
+[assembly: InternalsVisibleTo ("LibHoney.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010067ab48f95268b599e7e0c7b4cfa067950937a46b48f3559243c1d73f3a7103197448091d36ab978664fcd41fce9f1a83b0a6f139f7c46af82f8b09836048bea59ca8576f7b3138eb85763793694ec2ab7812ce9eb49f753ceb0fcb97698f117c7ee23e06fbe8b773a94880715844a03208811f49daba121d9e21f594a31a1d94")]
+#endif
