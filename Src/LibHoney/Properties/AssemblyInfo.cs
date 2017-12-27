b139434396ef7ed985a5e0f58bcbf41bdc8c18a4
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-12-27 03:34:07 -0600

    Bump version to 0.9.1.0

--- Src/LibHoney/Properties/AssemblyInfo.cs
+++ Src/LibHoney/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo ("LibHoney.Tests")]
